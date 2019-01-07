e204b8ec635c93dab76aa4d6085a929a846a8503
icewaryak30 <46440998+icewaryak30@users.noreply.github.com>
2019-01-07 15:36:52 +0530

    Create vowelcons.py

---
+++ vowelcons.py
@@ -0,0 +1,8 @@
+s=input()
+if s.isalpha():
+	if(s=="A" or s=="a" or s=="E" or s=="e" or s=="I" or s=="i" or s=="O" or s=="o" or s=="U" or s=="u"):
+		print("Vowel")
+	else:
+		print("Consonant")
+else:
+	print("invalid")
