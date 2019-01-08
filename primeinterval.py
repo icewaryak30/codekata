394da596e324366cb6b5938b007f1f3278b0d32a
icewaryak30 <46440998+icewaryak30@users.noreply.github.com>
2019-01-08 16:59:10 +0530

    Update primeinterval.py

--- primeinterval.py
+++ primeinterval.py
@@ -1,9 +1,15 @@
 n,k=map(int,input().split())
 c=0
+l=[]
 for b in range(n+1,k):
 	for j in range(2,b):
 		if b%j==0:
 			c=c+1
 			break
 	else:
-		print(b,end=" ")
+		l.append(b)
+for x in range(0,len(l)):
+	if x==0:
+		print(l[x],end="")
+	else:
+		print(" ",l[x],end="")
