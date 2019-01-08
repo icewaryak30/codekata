506d818871bef9b5dd0f62f2fe3eda32aff3478c
icewaryak30 <46440998+icewaryak30@users.noreply.github.com>
2019-01-08 16:48:52 +0530

    Update eveninterval.py

--- eveninterval.py
+++ eveninterval.py
@@ -1,5 +1,11 @@
 n,m=map(int,input().split())
+l=[]
 if n<=100000 and m<=100000:
 	for i in range(n+1,m):
 		if i%2==0:
-			print(i,end=" ")
+			l.append(i)
+	for k in range(0,len(l)):
+		if k==0:
+			print(l[k],end="")
+		else:
+			print(" ",l[k],end="")
