b41ea43eb85b0485f8594685c2e87c842979200b
icewaryak30 <46440998+icewaryak30@users.noreply.github.com>
2019-01-08 14:55:02 +0530

    Update sumk.py

--- sumk.py
+++ sumk.py
@@ -1,6 +1,7 @@
 n,k=map(int,input().split())
 x=[int(i) for i in input().split()]
-s=[]
-for i in range(0,k):
-	s.append(x[i])
-print(sum(s))
+if n>=k:
+	s=[]
+	for i in range(0,k):
+		s.append(x[i])
+	print(sum(s))
