6a8c3c82c01c528ea71ff77c06091044284538ff
icewaryak30 <46440998+icewaryak30@users.noreply.github.com>
2019-01-11 17:11:40 +0530

    Create digsep.py

---
+++ digsep.py
@@ -0,0 +1,11 @@
+n=int(input())
+l=[]
+while n>0:
+    r=n%10
+    l.append(r)
+    n=n//10
+for i in range(len(l),0,-1):
+    if i==len(l):
+        print(l[i-1],end=" ")
+    else:
+        print("",l[i-1],end="")
